f7b34ad5ff49f83f13db094fdf0d9b0fa7ceaa52
Alexander <91420542+Sadeal@users.noreply.github.com>
2022-03-20 22:15:14 +0800

    Create Main.java

---
+++ com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import UI.MainWindow;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	new MainWindow();
+    }
+}
